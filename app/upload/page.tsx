757859f0cadc70768815a8a7d110c54326f502bb
himangshu <himangshu73@gmail.com>
2025-11-24 23:02:38 +0600

    upload button

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+import { useState } from "react";
+
 export default function UploadPage() {
+  const [selectedFileName, setSelectedFileName] = useState("");
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
       <div className="w-full max-w-md bg-white shadow-lg rounded-2xl p-6 space-y-6">
@@ -11,9 +15,22 @@ export default function UploadPage() {
             Upload PDF
           </label>
           <input
+            id="fileUpload"
             type="file"
-            className="border rounded-xl p-2 text-sm bg-gray-50 hover:bg-blue-200 cursor-pointer"
+            className="hidden"
+            onChange={(e) =>
+              setSelectedFileName(e.target.files?.[0]?.name || "")
+            }
           />
+          <label
+            htmlFor="fileUpload"
+            className="cursor-pointer bg-blue-600 text-white p-3 rounded-xl text-center shadow hover:bg-blue-700 transition"
+          >
+            {selectedFileName ? selectedFileName : "Upload PDF"}
+          </label>
+          <p className="text-xs text-gray-500" id="fileNameDisplay">
+            {selectedFileName || "No file choosen."}
+          </p>
         </div>
         <div className="flex flex-col gap-1">
           <label className="text-sm font-medium text-gray-700">Category</label>
