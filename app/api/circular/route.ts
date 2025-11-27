de006f07f24a364d6ce48777831b89355ea8d8a3
himangshu <himangshu73@gmail.com>
2025-11-27 07:54:25 +0600

    upload api

---
+++ app/api/circular/route.ts
@@ -0,0 +1,74 @@
+import { auth } from "@/auth";
+import dbConnect from "@/lib/dbConnect";
+import Circular from "@/model/Circular";
+import { v2 as cloudinary } from "cloudinary";
+import { NextResponse } from "next/server";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+export async function POST(req: Request) {
+  try {
+    await dbConnect();
+
+    const session = await auth();
+    if (!session?.user?.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const formData = await req.formData();
+
+    const file = formData.get("file") as File | null;
+    const fileName = formData.get("fileName") as string;
+    const date = formData.get("date") as string;
+    const category = formData.get("category") as string;
+    const description = formData.get("description") as string;
+
+    if (!file) {
+      return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { folder: "circulars", resource_type: "auto" },
+          (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+          }
+        )
+        .end(buffer);
+    });
+
+    const upload: any = uploadResult;
+
+    const newCircular = await Circular.create({
+      fileName,
+      category,
+      date,
+      description,
+      cloudinaryUrl: upload.secure_url,
+      publicId: upload.public_id,
+      fileSize: upload.bytes,
+      format: upload.format,
+      uploadedBy: session.user.id,
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        circular: newCircular,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "Upload Failed" }, { status: 500 });
+  }
+}
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -3,9 +3,39 @@ import { useState } from "react";
 
 export default function UploadPage() {
   const [selectedFileName, setSelectedFileName] = useState("");
+  const [file, setFile] = useState<File | null>();
+
+  const [category, setCategory] = useState("GAD");
+  const [fileName, setFileName] = useState("");
+  const [circularDate, setCircularDate] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!file) return alert("Please upload a PDF");
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("category", category);
+    formData.append("fileName", fileName);
+    formData.append("date", circularDate);
+    formData.append("description", description);
+
+    const res = await fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    alert(data.message);
+  };
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
-      <div className="w-full max-w-md bg-white shadow-lg rounded-2xl p-6 space-y-6">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-md bg-white shadow-lg rounded-2xl p-6 space-y-6"
+      >
         <h1 className="text-2xl font-semibold text-gray-800 text-center">
           Upload Circular
         </h1>
@@ -19,9 +49,11 @@ export default function UploadPage() {
             type="file"
             className="hidden"
             required
-            onChange={(e) =>
-              setSelectedFileName(e.target.files?.[0]?.name || "")
-            }
+            onChange={(e) => {
+              const f = e.target.files?.[0] || null;
+              setFile(f);
+              setSelectedFileName(f?.name || "");
+            }}
           />
           <label
             htmlFor="fileUpload"
@@ -37,6 +69,8 @@ export default function UploadPage() {
           <label className="text-sm font-medium text-gray-700">Category</label>
           <select
             required
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
             className="border rounded-xl p-3 text-sm bg-gray-50 cursor-pointer"
           >
             <option value="GAD">GAD</option>
@@ -49,6 +83,8 @@ export default function UploadPage() {
           <input
             type="text"
             placeholder="Enter file name"
+            value={fileName}
+            onChange={(e) => setFileName(e.target.value)}
             required
             className="border rounded-xl p-3 text-sm bg-gray-50"
           />
@@ -60,6 +96,8 @@ export default function UploadPage() {
           <input
             type="date"
             required
+            value={circularDate}
+            onChange={(e) => setCircularDate(e.target.value)}
             className="border rounded-xl p-3 text-sm bg-gray-50"
           />
         </div>
@@ -69,6 +107,8 @@ export default function UploadPage() {
           </label>
           <textarea
             rows={3}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             placeholder="Write a short description..."
             className="border rounded-xl p-3 text-sm bg-gray-50 resize-none"
           ></textarea>
@@ -76,7 +116,7 @@ export default function UploadPage() {
         <button className="w-full bg-blue-600 text-white p-3 rounded-xl font-medium shadow hover:bg-blue-700 transition cursor-pointer">
           Upload
         </button>
-      </div>
+      </form>
     </div>
   );
 }
