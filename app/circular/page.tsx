269b7e3059e1ef749d22c816c346c90142f561a6
himangshu <himangshu73@gmail.com>
2025-11-28 20:17:34 +0600

    showcircular link

---
+++ app/circular/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface Circular {
+  public_id: string;
+  secure_url: string;
+}
+
+export default function CircularPage() {
+  const [circulars, setCirculars] = useState([]);
+  async function fetchPDFs() {
+    const res = await fetch("/api/showcircular");
+    const data = await res.json();
+    console.log(data);
+    setCirculars(data.circulars);
+  }
+  useEffect(() => {
+    fetchPDFs();
+  }, []);
+  return (
+    <div>
+      {circulars.map((c: Circular) => (
+        <div key={c.public_id}>
+          <a href={c.secure_url} target="_blank" rel="noopener noreferrer">
+            Link
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
