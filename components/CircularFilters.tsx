da72eb8516b9bbcfcdda8dc528912950fd2364bb
himangshu <himangshu73@gmail.com>
2025-12-11 08:21:09 +0600

    searchbox fix

--- components/CircularFilters.tsx
+++ components/CircularFilters.tsx
@@ -33,46 +33,46 @@ export default function CircularFilters({ onFilter }: CircularFilterProps) {
     <div className="w-full bg-gray-800 p-4 rounded-xl shadow-md">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col md:flex-row flex-wrap gap-4 items-start md:items-center "
+        className="grid grid-cols-1 md:grid-cols-5 gap-4 w-full "
       >
         <input
           type="text"
           placeholder="Type name or description"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="flex-1 min-w-[200px] bg-gray-700 text-gray-200 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full bg-gray-700 text-gray-200 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <select
           value={category}
           onChange={(e) => setCategory(e.target.value)}
-          className="min-w-[120px] bg-gray-700 text-gray-200 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full bg-gray-700 text-gray-200 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           <option value="">All</option>
           <option value="GAD">GAD</option>
           <option value="SME">SME</option>
           <option value="CIB">CIB</option>
         </select>
-        <div className="flex flex-col md:flex-row items-center gap-2 min-w-[150px]">
-          <label className="text-xs text-gray-400 mb-1`">From</label>
+        <div className="flex flex-col w-full">
+          <label className="text-xs text-gray-400">From</label>
           <input
             type="date"
             value={from}
             onChange={(e) => setFrom(e.target.value)}
-            className="bg-gray-700 text-gray-200 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full bg-gray-700 text-gray-200 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
-        <div className="flex items-center gap-2 min-w-[150px]">
-          <label className="text-xs text-gray-400 mb-1`">To</label>
+        <div className="flex flex-col w-full">
+          <label className="text-xs text-gray-400">To</label>
           <input
             type="date"
             value={to}
             onChange={(e) => setTo(e.target.value)}
-            className="bg-gray-700 text-gray-200 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full bg-gray-700 text-gray-200 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
         <button
           type="submit"
-          className="min-w-[120px] bg-blue-500 hover:bg-blue-700 text-white px-3 py-2 cursor-pointer rounded-lg shadow-md mt-2 md:mt-0"
+          className="w-full md:w-auto bg-blue-500 hover:bg-blue-700 text-white px-3 py-2 cursor-pointer rounded-lg shadow-md mt-2 md:mt-0"
         >
           Search
         </button>
