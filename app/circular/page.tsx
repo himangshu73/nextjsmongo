ec284a6b9c38e86be5d86641cdb3eb47141f0b3b
himangshu <himangshu73@gmail.com>
2026-01-01 08:44:59 +0600

    fix

--- app/circular/page.tsx
+++ app/circular/page.tsx
@@ -1,6 +1,7 @@
 import CircularList from "@/components/CircularList";
 import dbConnect from "@/lib/dbConnect";
 import Circular from "@/model/Circular";
+import Category from "@/model/Category";
 import { ICircular } from "@/types/circular";
 
 export default async function CircularPage() {
