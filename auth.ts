653cab11d3ec767509a0eba24a3aea53176f9b65
himangshu <himangshu73@gmail.com>
2026-01-20 22:15:06 +0600

    google auth

--- auth.ts
+++ auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import client from "./lib/db";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(client),
-  providers: [GitHub],
+  providers: [GitHub, Google],
   secret: process.env.AUTH_SECRET,
   callbacks: {
     async session({ session, user }) {
--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -4,11 +4,13 @@ import { signIn } from "next-auth/react";
 
 export default function SignIn() {
   return (
-    <button
-      onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
-      className="bg-green-700 hover:bg-green-800 px-3 py-2 rounded-md text-white cursor-pointer"
-    >
-      Login
-    </button>
+    <div>
+      <button
+        onClick={() => signIn(undefined, { callbackUrl: "/dashboard" })}
+        className="bg-green-700 hover:bg-green-800 px-3 py-2 rounded-md text-white cursor-pointer"
+      >
+        Login
+      </button>
+    </div>
   );
 }
