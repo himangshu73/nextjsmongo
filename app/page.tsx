143af649e892ba7691c7ee7221c719df4623bae0
himangshu <himangshu73@gmail.com>
2026-03-24 07:36:42 +0600

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div className="p-2">HomePage</div>;
+import CircularList from "@/components/CircularList";
+import Circular from "@/model/Circular";
+import { ICircular } from "@/types/circular";
+
+export default async function Home() {
+  const circulars = await Circular.find()
+    .populate("category", "name")
+    .sort({ date: -1 })
+    .limit(5)
+    .lean<ICircular[]>();
+  return (
+    <div className="p-2">
+      <CircularList circulars={circulars} />
+    </div>
+  );
 }
