9e7ff96ffb40bbc78b83e18d631e4e3280edbc51
himangshu <himangshu73@gmail.com>
2026-06-09 08:41:23 +0600

    categorylist page

--- app/categorylist/page.tsx
+++ app/categorylist/page.tsx
@@ -1,3 +1,17 @@
-export default function CategoryList() {
-  return <div>Categorylist Page</div>;
+import CategoryList from "@/components/CategoryList";
+import dbConnect from "@/lib/dbConnect";
+import Category from "@/model/Category";
+import { ICategory } from "@/types/circular";
+
+export default async function CategoryListPage() {
+  await dbConnect();
+
+  const foundCategories = await Category.find().lean<ICategory[]>();
+
+  const categories = JSON.parse(JSON.stringify(foundCategories));
+  return (
+    <div>
+      <CategoryList categories={categories} />
+    </div>
+  );
 }
